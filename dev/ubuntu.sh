6c565a2a8e4c9ac40b6b8d46cec4c5ef1c855f3a
Johannes Eckhoff <j.eckhoff@gmail.com>
2024-12-18 09:36:53 +0100

    Add xorg
    Missing glfw dependencie missing from dev script

--- dev/ubuntu.sh
+++ dev/ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install -y libxinerama-dev libxcursor-dev libglu1-mesa pkg-config
+sudo apt install -y libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa pkg-config
  
 # glfw dependencies
 # xinerama
